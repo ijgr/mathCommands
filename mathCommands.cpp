2eb635650bbbc2f051332d190fca0dec8a57c65c
Irene <35508977+ijgr@users.noreply.github.com>
2019-03-06 18:29:33 -0800

    Create mathCommands.cpp

---
+++ mathCommands.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#define E  2.71828
+using namespace std;
+
+int main()
+{
+	cout << "abs(-23.7) = " << abs(-23.7) << endl;
+	cout << "abs(0.0) = "<< abs(0.0) << endl;
+	cout << "abs(-23.7) =" <<abs(-23.7) << endl;
+	cout << "ceil(9.2) = " << ceil(9.2) << endl;
+	cout << "ceil(-9.8) = " << ceil(-9.8) << endl;
+	cout << "cos(0.0) = " <<cos(0.0) << endl;
+	cout << "exp(1.0) = " << exp(1.0) << endl;
+	cout << "exp(2.0) = " << exp(2.0) << endl;
+	cout << "floor(9.2) = " << floor(9.2) << endl;
+	cout << "floor(-9.8) = " << floor(-9.8) << endl;
+	cout << "log(E) = " << log(E) << endl;
+	cout << "log(E * E) = " <<
+		log(E * E) << endl;
+	cout << "max(2.3, 12.7) = " << max(2.3,12.7) << endl;
+	cout << "max(-2.3, -12.7) = " <<
+		max(-2.3, -12.7) << endl;
+	cout << "min(2.3, 12.7) = " << min(2.3,12.7) << endl;
+	cout << "min(-2.3, -12.7) = " <<
+		min(-2.3,-12.7) << endl;
+	cout << "pow(2.0, 7.0) = " << pow(2.0, 7.0) << endl;
+	cout << "pow(9.0, 0.5) = " << pow(9.0, 0.5) << endl;
+	cout << "sin(0.0) = " << sin(0.0) << endl;
+	cout << "sqrt(900.0) = " <<sqrt(900.0) << endl;
+	cout << "tan(0.0) = " << tan(0.0) << endl;
+	system("pause");
+	return 0;
+}
